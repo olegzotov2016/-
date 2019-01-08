1db3d53afdbf73245d82056195c8916b95ffcea7
oleg <olegzotov2016@gmail.com>
2019-01-09 01:34:14 +0400

    новый класс ListInherited

--- venv/lister.py
+++ venv/lister.py
@@ -20,7 +20,7 @@ class ListerInstance:
     """
 
     def __str__(self):
-        return '<Instance of %s, address %s:\n%s>'%(
+        return '<Instance of %s, address %s:\n%s>' % (
             self.__class__.__name__,
             id(self),
             self.__attrnames())
@@ -29,4 +29,32 @@ class ListerInstance:
         result = ''
         for attr in sorted(self.__dict__):
             result += '\tname %s=%s\n' % (attr, self.__dict__[attr])
+        return result
+
+
+class ListInherited:
+    """
+    Использует функцию dir() для получения списка атрибутов самого экземпляра
+    и атрибутов, унаследованных экземпляром от его классов; в Python 3.0
+    выводится больше имен атрибутов, чем в 2.6, потому что классы нового стиля
+    в конечном итоге наследуют суперкласс object; метод getattr() позволяет
+    получить значения унаследованных атрибутов, отсутствующих в self.__dict__;
+    реализует метод __str__, а не __repr__, потому что в противном случае
+    данная операция может попасть в бесконечный цикл при выводе связанных
+    методов!
+    """
+
+    def __str__(self):
+        return '<Instance of %s, address %s:\n%s' % (
+            self.__class__.__name__,
+            id(self),
+            self.__attrnames())
+
+    def __attrnames(self):
+        result = ''
+        for attr in dir(self):
+            if attr[:2] == '__' and attr[-2:] == '__':
+                result += '\tname %s=<>\n' % attr
+            else:
+                result += '\tname %s=%s\n' % (attr, getattr(self, attr))
         return result
